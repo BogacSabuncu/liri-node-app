0d6005e1c3334e18863fc2742bd37b15086eb92e
BogacSabuncu <bsabuncu@mac.com>
2019-05-14 19:46:37 -0400

    Added the function to get the movie information from OMBD

--- liri.js
+++ liri.js
@@ -9,7 +9,7 @@ let artist = "Ariana Grande";
 function getConcert(artist) {
 
     //request URL
-    let URL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+    let URL = encodeURI("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp");
 
     axios.get(URL)//axios request for the artist
         .then(function (response) {
@@ -26,9 +26,9 @@ function getConcert(artist) {
                 console.log(`   The venue is at: ${venue.city}, ${venue.country}`);
                 console.log(`   The time of the venue is: ${time}`);
             }
-            else { 
+            else {
                 //if no concerts are found display this
-                console.log("Couldn't find any concerts for this artist!");
+                console.log("Sorry couldn't find any concerts for this artist!");
             }
         })
         .catch(function (error) {
@@ -36,21 +36,31 @@ function getConcert(artist) {
         });
 }
 
-// movieTitle = "Mr Nobody"
-// let URL = "http://www.omdbapi.com/?apikey=trilogy&t"+movieTitle;
+function getMovie(movieTitle) {
+    let URL = encodeURI("http://www.omdbapi.com/?apikey=trilogy&t=" + movieTitle);
 
-// axios.get(URL)//axios request for the artist
-// .then(function (response) {
+    axios.get(URL)//axios request for the artist
+        .then(function (response) {
 
-//     //if there are available concerts
-//     if (response.data.length > 0) {
-//        console.log(response);
-//     }
-//     else { 
-//         //if no concerts are found display this
-//         console.log("Couldn't find any concerts for this artist!");
-//     }
-// })
-// .catch(function (error) {
-//     console.log(error);
-// });
+            //if there is no movie for that title
+            if (response.data.Response === "False") {
+                console.log("Sorry couldn't find any movies with that title!")
+            }
+            else {
+                let movie = response.data;
+                console.log("Here is the information you are looking for:");
+                console.log(`   Movie Title: ${movie.Title}`);
+                console.log(`   Year Released: ${movie.Year}`);
+                console.log(`   IMDB Rating: ${movie.Ratings[0].Value}`);
+                console.log(`   Rotten Tomatoes Rating: ${movie.Ratings[1].Value}`);
+                console.log(`   Country: ${movie.Country}`);
+                console.log(`   Languages: ${movie.Language}`);
+                console.log(`   Plot: ${movie.Plot}`);
+                console.log(`   Actors: ${movie.Actors}`);
+            }
+
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
